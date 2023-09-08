f418b09e6d54475421d8f723711394551d47cd2f
Leonardo <leonardojfs83@gmail.com>
2023-09-07 23:39:13 -0300

    imports en main

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState } from "react";
 
-const [first, setfirst] = useState(second)
 
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import App from './App.jsx'
+import Home from './Routes/Home.jsx'
+import Contact from './Routes/Contact.jsx'
+import Favs from './Routes/Favs.jsx'
+import Detail from './Routes/Detail.jsx'
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
